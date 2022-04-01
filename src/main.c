e3abe73bc168429ccc728540ef40d55a8d102106
surui <99946023+su007rui@users.noreply.github.com>
2022-04-01 06:08:14 +0000

    Create structure data

--- src/main.c
+++ src/main.c
@@ -1,8 +1,28 @@
 /* hello.c */
 #include <stdio.h>
 
-void main (void)
+/*Sourse fils with structure
+*studentDate
+*/
+
+struct StudentData{
+char *stu_name;
+int stu_id;
+int stu_age;
+};
+int main ()
 {
-	printf ("Hello World\n");
-	printf ("Goodbye World\n");
+  /*student is the varible of stucture StudentData*/
+  struct StudentData student;
+  /*Assinging the valuse to StudentData*/
+  student.stu_name="SuRui";
+  student.stu_id=20102000;
+  student.stu_age=21;
+
+  /*Displaying the values of struct nember*/
+  printf("Hello,%s",student.stu_name);
+  printf("\nStudent Id is: %d" ,student.stu_id);
+  printf("\nStudent Age is: %d",student.stu_age);
+  printf("\nGoodbye!");
+  return 0;
 }
